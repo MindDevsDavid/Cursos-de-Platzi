5fdf6d76f2f2970b39ce30607a347fbdb393a9f7
MindDevsDavid <77819511+MindDevsDavid@users.noreply.github.com>
2021-01-22 16:45:24 -0500

    Add files via upload

---
+++ Curso de introduccion a C++/Proyecto1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int dia = 8;
+  int mes = 7;
+  int anio = 2003;
+  char nombre [9] = "David";
+
+  cout << "Mi cumpleanios es:"<<endl;
+  cout << dia << " Martes" << endl;
+  cout << mes << " Julio" << endl;
+  cout << anio << " Dos mil tres" << endl;
+  cout << "Mi nombre es " << nombre << endl;
+
+    return 0;
+}
